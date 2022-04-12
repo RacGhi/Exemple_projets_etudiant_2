2cd939e5f843144c8a82898e00ac3046dfa8d99e
RacGhi <64044326+RacGhi@users.noreply.github.com>
2022-04-11 20:46:29 -0400

    Update dash.py

--- dash.py
+++ dash.py
@@ -12,7 +12,7 @@ end_date =  datetime.date.today()
 
 
 # Page setting
-#st.set_page_config(layout="wide")
+st.set_page_config(layout="wide")
 
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
